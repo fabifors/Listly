7ab1ea6d8320c7c0fde206932debdb534432d910
fabifors <fabian.forsstrom@gmail.com>
2019-11-02 19:15:01 +0100

    Added testing view to test new todos API

---
+++ src/views/Testing.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout-section">
+    <todo-testing />
+  </div>
+</template>
+
+<script>
+import todos from '@/store/modules/Todos'
+
+export default {
+  name: 'Testing',
+  components: {
+    'todo-testing': todos.view 
+  },
+  beforeCreate () {
+    this.$store.dispatch('todos/initTodos', { root: true });
+  }
+}
+</script>
