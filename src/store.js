2c382cbb01e882fc13d293efcc253c63f8b10cb4
fabifors <fabian.forsstrom@gmail.com>
2019-11-02 15:50:30 +0100

    Removed INITIAL_STATE object from original store

--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import router from './router';
+import router from '@/router';
 
-import { hash } from './functions';
+import hash from '@/utilities/hash';
+import INITIAL_STATE from '@/INITIAL_STATE';
 
 Vue.use(Vuex);
-
-
 
 export default new Vuex.Store({
   state: {
@@ -224,6 +223,7 @@ export default new Vuex.Store({
     updateTimestamp ({ commit }, listId) {
       const timestamp = new Date().getTime();
       commit('UPDATE_TIMESTAMP', { listId, timestamp });
+      dispatch('updateStorage');
     },
 
     reorderLists ({ commit, dispatch }, lists) {
@@ -284,124 +284,3 @@ export default new Vuex.Store({
     }
   }
 });
-
-const INITIAL_STATE = {
-  theme: {
-    color: 70,
-    dark_mode: false
-  },
-  currentList: '',
-  categories: {
-    'kas2d-ad-a2d-a2d-2ad': {
-      id: 'kas2d-ad-a2d-a2d-2ad',
-      name: 'Daily',
-      lists: ['as2-asd2-asd3-k2-asd2']
-    },
-    'kas2d-ad-a2d-a2d-2ae': {
-      id: 'kas2d-ad-a2d-a2d-2ae',
-      name: 'Lök',
-      lists: ['as2-asd2-asd3-k2-asd4']
-    }
-  },
-  lists: {
-    'as2-asd2-asd3-k2-asd2': {
-      id: 'as2-asd2-asd3-k2-asd2',
-      title: 'An Awesome List',
-      category: 'kas2d-ad-a2d-a2d-2ad',
-      todos: [
-        {
-          content: 'Code like a monkey',
-          done: false,
-          editing: false,
-          id: hash()
-        }, 
-        {
-          content: 'Pet the cat',
-          done: false,
-          editing: false,
-          id: hash()
-        },
-        {
-          content: 'Take a dump',
-          done: false,
-          editing: false,
-          id: hash()
-        },
-        { 
-          content: 'Make something useless',
-          done: true,
-          editing: false,
-          id: hash()
-        }
-      ],
-      created: new Date().getTime(),
-      updated: new Date().getTime()
-    },
-    'as2-asd2-asd3-k2-asd4': {
-      id: 'as2-asd2-asd3-k2-asd4',
-      title: 'An Awesome List',
-      category: 'kas2d-ad-a2d-a2d-2ae',
-      todos: [
-        {
-          content: 'Code like a monkey',
-          done: false,
-          editing: false,
-          id: hash()
-        }, 
-        {
-          content: 'Pet the cat',
-          done: false,
-          editing: false,
-          id: hash()
-        },
-        {
-          content: 'Take a dump',
-          done: false,
-          editing: false,
-          id: hash()
-        },
-        { 
-          content: 'Make something useless',
-          done: true,
-          editing: false,
-          id: hash()
-        }
-      ],
-      created: new Date().getTime(),
-      updated: new Date().getTime()
-    },
-    'as2-asd2-asd3-k2-asd1': {
-      id: 'as2-asd2-asd3-k2-asd1',
-      title: 'An Awesome List',
-      category: '',
-      todos: [
-        {
-          content: 'Code like a monkey',
-          done: false,
-          editing: false,
-          id: hash()
-        }, 
-        {
-          content: 'Pet the cat',
-          done: false,
-          editing: false,
-          id: hash()
-        },
-        {
-          content: 'Take a dump',
-          done: false,
-          editing: false,
-          id: hash()
-        },
-        { 
-          content: 'Make something useless',
-          done: true,
-          editing: false,
-          id: hash()
-        }
-      ],
-      created: new Date().getTime(),
-      updated: new Date().getTime()
-    }
-  }
-};
